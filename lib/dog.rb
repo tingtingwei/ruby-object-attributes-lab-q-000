c715e3ab9007b6ac0cfe27b48f7ef90e197e45a2
tingtingwei <ttw704@gmail.com>
2016-02-25 20:25:10 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  #writes
+  def name=(temp_variable)
+    @name = temp_variable
+  end
+
+  #reads
+  def name
+    @name
+  end
+
+  def breed=(temp)
+    @breed = temp
+  end
+
+  def breed
+    @breed
+  end
+
+end
